820078a0cd621458c9e2841d943ac6a3223dd7f6
wangxiao <wbnupku@gmail.com>
2018-08-07 11:14:39 +0800

    add rs2_multi_aligh.py

--- align.py
+++ align.py
@@ -1,5 +1,5 @@
-## License: Apache 2.0. See LICENSE file in root directory.
-## Copyright(c) 2017 Intel Corporation. All Rights Reserved.
+# License: Apache 2.0. See LICENSE file in root directory.
+# Copyright(c) 2017 Intel Corporation. All Rights Reserved.
 
 #####################################################
 ##              Align Depth to Color               ##
@@ -15,7 +15,7 @@ import cv2
 # Create a pipeline
 pipeline = rs.pipeline()
 
-#Create a config and configure the pipeline to stream
+# Create a config and configure the pipeline to stream
 #  different resolutions of color and depth streams
 config = rs.config()
 # config.enable_stream(rs.stream.depth, 640, 360, rs.format.z16, 30)
@@ -31,18 +31,17 @@ profile = pipeline.start(config)
 # Getting the depth sensor's depth scale (see rs-align example for explanation)
 depth_sensor = profile.get_device().first_depth_sensor()
 depth_scale = depth_sensor.get_depth_scale()
-print "Depth Scale is: " , depth_scale
+print "Depth Scale is: ", depth_scale
 
 # We will be removing the background of objects more than
 #  clipping_distance_in_meters meters away
-clipping_distance_in_meters = 1 #1 meter
+clipping_distance_in_meters = 1  # 1 meter
 clipping_distance = clipping_distance_in_meters / depth_scale
 
 # Create an align object
 # rs.align allows us to perform alignment of depth frames to others frames
 # The "align_to" is the stream type to which we plan to align depth frames.
-align_to = rs.stream.color
-align = rs.align(align_to)
+align = rs.align(rs.stream.color)
 
 # Streaming loop
 count = 0
@@ -58,28 +57,27 @@ try:
             # Get frameset of color and depth
             frames = pipeline.wait_for_frames()
             # frames.get_depth_frame() is a 640x360 depth image
-            
+
             # Align the depth frame to color frame
             aligned_frames = align.process(frames)
-            
+
             # Get aligned frames
-            aligned_depth_frame = aligned_frames.get_depth_frame() # aligned_depth_frame is a 640x480 depth image
+            # aligned_depth_frame is a 640x480 depth image
+            aligned_depth_frame = aligned_frames.get_depth_frame()
             color_frame = aligned_frames.get_color_frame()
-            
+
             # Validate that both frames are valid
             if not aligned_depth_frame or not color_frame:
                 continue
-            
+
             depth_image = np.asanyarray(aligned_depth_frame.get_data())
             color_image = np.asanyarray(color_frame.get_data())
-       
+
             with open('test_align/%d-%d_Depth.raw' % (count, i), 'wb') as fout:
                 fout.write(depth_image.astype(np.uint16).tobytes())
             cv2.imwrite('test_align/%d_Color.png' % count, color_image)
         count += 1
-      
-        
+
 
 finally:
     pipeline.stop()
-
---
+++ rs2_multi_align.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+# Copyright (c) 2016 Baidu.com, Inc. All Rights Reserved
+#
+###############################################################################
+"""
+Give out .
+
+Authors: wangxiao05(wangxiao05@baidu.com)
+Date:    2018/08/06 15:32:43
+"""
+import pyrealsense2 as rs
+import numpy as np
+import cv2
+import os
+import time
+
+
+# get devices
+ctx = rs.context()
+devices = ctx.query_devices()
+sn_list = []
+
+for d in devices:
+    sn = d.get_info(rs.camera_info.serial_number)
+    try:
+        sn_num = int(sn)
+        sn_list.append(sn)
+        if len(sn_list) >= 2:
+            break
+    except:
+        continue
+assert len(sn_list) >= 2, 'len sn_list: {}'.format(len(sn_list))
+
+
+# Configure depth and color streams...
+# ...from Camera 1
+pipeline_1 = rs.pipeline()
+config_1 = rs.config()
+config_1.enable_device(sn_list[0])
+config_1.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+config_1.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+# ...from Camera 2
+pipeline_2 = rs.pipeline()
+config_2 = rs.config()
+config_2.enable_device(sn_list[1])
+config_2.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+config_2.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+
+
+# Start streaming from both cameras
+pipeline_1.start(config_1)
+pipeline_2.start(config_2)
+
+align1 = rs.align(rs.stream.color)
+align2 = rs.align(rs.stream.color)
+
+cam_dir_1 = 'depth_cam1'
+cam_dir_2 = 'depth_cam2'
+if not os.path.exists(cam_dir_1):
+    os.makedirs(os.path.join(cam_dir_1, 'depth'))
+    os.makedirs(os.path.join(cam_dir_1, 'color'))
+if not os.path.exists(cam_dir_2):
+    os.makedirs(os.path.join(cam_dir_2, 'depth'))
+    os.makedirs(os.path.join(cam_dir_2, 'color'))
+try:
+    while True:
+
+        tm = str(int(time.time() * 1000))
+        # Camera 1
+        # Wait for a coherent pair of frames: depth and color
+        frames_1 = pipeline_1.wait_for_frames()
+        aligned_frames_1 = align1.process(frames_1)
+        depth_frame_1 = aligned_frames_1.get_depth_frame()
+        color_frame_1 = aligned_frames_1.get_color_frame()
+        if not depth_frame_1 or not color_frame_1:
+            continue
+        # Convert images to numpy arrays
+        depth_image_1 = np.asanyarray(depth_frame_1.get_data())
+        color_image_1 = np.asanyarray(color_frame_1.get_data())
+
+        # Camera 2
+        # Wait for a coherent pair of frames: depth and color
+        frames_2 = pipeline_2.wait_for_frames()
+        aligned_frames_2 = align1.process(frames_2)
+
+        depth_frame_2 = aligned_frames_2.get_depth_frame()
+        color_frame_2 = aligned_frames_2.get_color_frame()
+        if not depth_frame_2 or not color_frame_2:
+            continue
+        # Convert images to numpy arrays
+        depth_image_2 = np.asanyarray(depth_frame_2.get_data())
+        color_image_2 = np.asanyarray(color_frame_2.get_data())
+        # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
+        depth_path_1 = os.path.join(cam_dir_1, 'depth', '%s.png' % tm)
+        color_path_1 = os.path.join(cam_dir_1, 'color', '%s.png' % tm)
+        depth_path_2 = os.path.join(cam_dir_2, 'depth', '%s.png' % tm)
+        color_path_2 = os.path.join(cam_dir_2, 'color', '%s.png' % tm)
+
+        with open(depth_path_1, 'wb') as fout:
+            fout.write(depth_image_1.astype(np.uint16).tobytes())
+        cv2.imwrite(color_path_1, color_image_1)
+        with open(depth_path_2, 'wb') as fout:
+            fout.write(depth_image_2.astype(np.uint16).tobytes())
+        cv2.imwrite(color_path_2, color_image_2)
+        ch = cv2.waitKey(200)
+
+finally:
+    # Stop streaming
+    pipeline_1.stop()
+    pipeline_2.stop()
