77cc892deaba4c9a3d3cba2b079ec5016817293c
wangxiao05 <wangxiao05@baidu.com>
2018-09-30 12:04:30 +0800

    fix gbackend

---
+++ gbackend/get_cam.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+# Copyright (c) 2016 Baidu.com, Inc. All Rights Reserved
+#
+###############################################################################
+"""
+Wrapper for actual worker.
+
+Authors: wangxiao05(wangxiao05@baidu.com)
+Date:    2018/07/23 14:55:56
+"""
+
+import base64
+
+
+def test():
+    import requests
+    url = 'http://127.0.0.1:8950/realsense_api/'
+    r = requests.get(url)
+    print 'Response:'
+    print r.json().keys()
+
+
+if __name__ == '__main__':
+    test()
